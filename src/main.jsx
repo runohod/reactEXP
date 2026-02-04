b2a9092ab1a09889dbe5365425924fe5848f5928
runohod <andrei.rumynthev1728@yandex.ru>
2026-02-04 19:18:42 +0300

    +

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import './styles'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Home from './pages/Home/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home />
   </StrictMode>,
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+const Home = () => { 
+    return (
+        <>
+            <h1 className="title">TODO LIST</h1>
+        </>
+    )
+}
+
+export default Home
