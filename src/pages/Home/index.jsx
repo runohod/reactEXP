0101f4d878493ffa0dfc4996e62386392ebb9f89
runohod <andrei.rumynthev1728@yandex.ru>
2026-02-09 18:55:50 +0300

    можно добавлять задачи через "+" и enter

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,22 +8,36 @@ import './index.scss';
 const Home = () => { 
   const [searchQuery, setSearchQuery] = useState('');
 
-  const tasks = [
-    { id: 'task-1', text: 'NOTE #1' },
-    { id: 'task-2', text: 'NOTE #2' },
-    { id: 'task-3', text: 'NOTE #3' },
-  ];
+  const [tasks, setTasks ] = useState([
+    { id: 'task-1', text: 'NOTE #1', isDone: false},
+    { id: 'task-2', text: 'NOTE #2', isDone: true},
+    { id: 'task-3', text: 'NOTE #3', isDone: true},
+  ]);
 
   const handleDelete = (id) => console.log('Задача удалена:', id);
   const handleEdit = (id) => console.log('Редактируем задачу:', id);
 
+  const addTask = (e) => {
+    e.preventDefault(); 
+    if (!searchQuery.trim()) return; 
+
+    const newTask = {
+      id: crypto.randomUUID(),
+      text: searchQuery,
+      isDone: false 
+    };
+
+    setTasks([...tasks, newTask]); 
+    setSearchQuery(''); 
+  };
+
     return (
     <div className="todo">
       <h1 className="todo__title">TODO LIST</h1>
-      <form className="todo__field field">
-        <SearchField placeholder="Search note..." iconColor="#6C63FF" value={searchQuery} onChange={(e) => {setSearchQuery(e.target.value); console.log("Введено:", e.target.value);}} />
-        <Button className="button__all" onClick={(e) => {e.preventDefault(); console.log("Выбор всех задач");}}>ALL</Button>
-        <Button className="button__theme" onClick={(e) => {e.preventDefault(); console.log("Тема сменилась");}}><MoonIcon color="#F7F7F7"/></Button>
+      <form className="todo__field field" onSubmit={addTask}>
+        <SearchField placeholder="Search note..." iconColor="#6C63FF" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={(e) => { if (e.key === 'Enter') { addTask(e);}}}/>
+        <Button type="button" className="button__all" onClick={(e) => {e.preventDefault(); console.log("Выбор всех задач");}}>ALL</Button>
+        <Button type="button" className="button__theme" onClick={(e) => {e.preventDefault(); console.log("Тема сменилась");}}><MoonIcon color="#F7F7F7"/></Button>
       </form>
 
       <TodoList 
@@ -31,6 +45,8 @@ const Home = () => {
         onDelete={handleDelete} 
         onEdit={handleEdit} 
       />
+
+      <Button className="button__add" onClick={addTask}>+</Button>
     </div>
   )
 }
