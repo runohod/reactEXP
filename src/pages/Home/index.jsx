a903696d94bbf0e896e23ca1b52077ce8f821600
runohod <andrei.rumynthev1728@yandex.ru>
2026-02-11 14:15:09 +0300

    +

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import ModalOpen from '../../ui/ModalOpen/ModalOpen.jsx'
 import TodoList from '../../ui/TodoList/TodoList.jsx'
 import SearchField from '../../ui/SearchField/SearchField.jsx'
 import MoonIcon from '../../ui/Icons/MoonIcon.jsx'
@@ -7,7 +8,8 @@ import './index.scss';
 
 const Home = () => { 
   const [searchQuery, setSearchQuery] = useState('');
-  const [editId, setEditId] = useState('');
+  // const [editId, setEditId] = useState('');
+  const [newTaskText, setNewTaskText] = useState('');
 
   const [tasks, setTasks ] = useState([
     { id: 'task-1', text: 'NOTE #1', isDone: true},
@@ -15,35 +17,17 @@ const Home = () => {
     { id: 'task-3', text: 'NOTE #3', isDone: false},
   ]);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {setIsModalOpen(true);};
+
   const handleDelete = (id) => {setTasks(tasks.filter(task => task.id !== id));};
-  const handleEdit = (id) => {
-  const taskToEdit = tasks.find(task => task.id === id);
-    if (taskToEdit) {
-      setSearchQuery(taskToEdit.text); 
-      setEditId(id);
-    }
-  };
-
-  const addTask = (e) => {
-      if (e) e.preventDefault(); 
-      if (!searchQuery.trim()) return; 
-
-      if (editId) {
-        setTasks(tasks.map(task => 
-          task.id === editId ? { ...task, text: searchQuery } : task
-        ));
-        setEditId(''); 
-      } else {
-        const newTask = {
-          id: crypto.randomUUID(),
-          text: searchQuery,
-          isDone: false 
-        };
-        setTasks([...tasks, newTask]); 
-      }
-
-      setSearchQuery(''); 
-  };
+  // const handleEdit = (id) => {
+  // const taskToEdit = tasks.find(task => task.id === id);
+  //   if (taskToEdit) {
+  //     setSearchQuery(taskToEdit.text); 
+  //     setEditId(id);
+  //   }
+  // };
 
   const handleToggle = (id) => {
     setTasks(tasks.map(task => 
@@ -51,13 +35,29 @@ const Home = () => {
     ));
   };
 
+  const handleCancel = () => {
+  setIsModalOpen(false);
+  setNewTaskText('');
+  };
 
+  const addTask = () => {
+    if (!newTaskText.trim()) return;
+
+    const newTask = {
+      id: crypto.randomUUID(),
+      text: newTaskText,
+      isDone: false
+    };
+
+    setTasks([...tasks, newTask]);
+    handleCancel(); 
+  };
 
     return (
     <div className="todo">
       <h1 className="todo__title">TODO LIST</h1>
-      <form className="todo__field field" onSubmit={addTask}>
-        <SearchField placeholder="Search note..." iconColor="#6C63FF" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={(e) => { if (e.key === 'Enter') { addTask(e);}}}/>
+      <form className="todo__field field">
+        <SearchField placeholder="Search note..." iconColor="#6C63FF" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
         <Button type="button" className="button__all" onClick={(e) => {e.preventDefault(); console.log("Выбор всех задач");}}>ALL</Button>
         <Button type="button" className="button__theme" onClick={(e) => {e.preventDefault(); console.log("Тема сменилась");}}><MoonIcon color="#F7F7F7"/></Button>
       </form>
@@ -65,11 +65,21 @@ const Home = () => {
       <TodoList 
         tasks={tasks}
         onDelete={handleDelete} 
-        onEdit={handleEdit} 
+        // onEdit={handleEdit} 
         onToggle={handleToggle}
       />
 
-      <Button className="button__add" onClick={addTask}>{editId !== '' ? '✓' : '+'}</Button>
+      <Button className="button__add" onClick={openModal}>+</Button>
+      
+      {isModalOpen && (
+        <ModalOpen 
+          value={newTaskText} 
+          onChange={(e) => setNewTaskText(e.target.value)} 
+          onKeyDown={(e) => { if (e.key === 'Enter') { addTask(e);}}}
+          onCancel={handleCancel} 
+          onApply={addTask} 
+        />
+      )}
     </div>
   )
 }
---
+++ src/ui/ModalOpen/ModalOpen.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from 'react';
+import './ModalOpen.scss'
+
+const ModalOpen = ({
+  placeholder="Input your note..." ,
+  value, 
+  onChange,
+  onKeyDown,
+  onCancel,
+  onApply,
+
+}) => { 
+    
+const inputRef = useRef(null);
+
+useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+}, []);
+
+  return (
+    <div className="modal">
+        <div className="modal__content">
+            <h2 className="modal__title">NEW NOTE</h2>
+                <input 
+                    ref={inputRef}
+                    className="modal__input"
+                    type="text" 
+                    placeholder={placeholder}
+                    value={value} 
+                    onChange={onChange} 
+                    onKeyDown={onKeyDown}
+                />
+            <div className="modal__actions">
+                <button className="modal__btn__cancel" onClick={onCancel}>CANCEL</button>
+                <button className="modal__btn__apply" onClick={onApply}>APPLY</button>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default ModalOpen;
+
+      
