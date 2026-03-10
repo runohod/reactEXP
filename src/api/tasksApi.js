a17656150f89a5c8c8270c58c0c5de1c2109e33d
runohod <andrei.rumynthev1728@yandex.ru>
2026-03-10 22:20:09 +0300

    12: tasksApi переписан на асинхронный метод async await

--- src/api/tasksApi.js
+++ src/api/tasksApi.js
@@ -3,90 +3,86 @@ class tasksApi {
 _url = import.meta.env.VITE_API_URL;
 _headers = {'Content-Type': 'application/json',};
 
-  getAll () {
-    return fetch(this._url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Ошибка сервера: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        console.error("Не удалось загрузить задачи:", error.message);
-        throw error;
-      });
+  async getAll () {
+    try {
+      const response = await fetch(this._url);
+      if (!response.ok) {
+        throw new Error(`Ошибка сервера: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Не удалось загрузить задачи:", error.message);
+      throw error;
+    }
   }
 
-  add (task) {
-    return fetch(this._url, {
-      method: 'POST',
-      headers: this._headers,
-      body: JSON.stringify(task)
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Ошибка сервера: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        console.error("Не удалось загрузить задачи:", error.message);
-        throw error;
+  async add (task) {
+    try {
+      const response = await fetch(this._url, {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify(task)
       });
+      if (!response.ok) {
+        throw new Error(`Ошибка сервера: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Не удалось загрузить задачи:", error.message);
+      throw error;
+    }
   }
 
-  delete (id) {
-    return fetch(`${this._url}/${id}`, {
-      method: 'DELETE',
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Ошибка сервера: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        console.error("Не удалось загрузить задачи:", error.message);
-        throw error;
+  async delete (id) {
+    try {
+      const response = await fetch(`${this._url}/${id}`, {
+        method: 'DELETE',
       });
+      if (!response.ok) {
+        throw new Error(`Ошибка сервера: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Не удалось загрузить задачи:", error.message);
+      throw error;
+    }
   }
 
-  toggleComplete (id, isDone) {
-    return fetch(`${this._url}/${id}`, {
-      method: 'PATCH',
-      headers: this._headers,
-      body: JSON.stringify({ isDone })
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Ошибка сервера: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        console.error("Не удалось загрузить задачи:", error.message);
-        throw error;
+  async toggleComplete (id, isDone) {
+    try {
+      const response = await fetch(`${this._url}/${id}`, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({ isDone })
       });
+      if (!response.ok) {
+        throw new Error(`Ошибка сервера: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Не удалось загрузить задачи:", error.message);
+      throw error;
+    }
   }
 
-  updateText (id, newText) {
-    return fetch(`${this._url}/${id}`, {
-      method: 'PATCH',
-      headers: this._headers,
-      body: JSON.stringify({ text: newText }) 
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Ошибка сервера: ${response.status}`);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        console.error("Не удалось загрузить задачи:", error.message);
-        throw error;
+  async updateText (id, newText) {
+    try {
+      const response = await fetch(`${this._url}/${id}`, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({ text: newText })
       });
+      if (!response.ok) {
+        throw new Error(`Ошибка сервера: ${response.status}`);
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Не удалось загрузить задачи:", error.message);
+      throw error;
+    }
   }
 }
 
 const api = new tasksApi();
 export default api;
+
